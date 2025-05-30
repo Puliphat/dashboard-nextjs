2f2a86cf226ad2d4c28bbcb88269a14f55c3baf5
Puliphat <myty.2543@gmail.com>
2025-05-30 15:24:13 +0700

    fix return auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,74 +2,74 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectMongoDB } from "../../../../../lib/mongodb";
 import User from "../../../../../models/user";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 
 const authOptions = {
-    providers: [
-        CredentialsProvider({
-          name: 'credentials',
-          credentials: {
-          },
-          async authorize(credentials, req) {
-           
-            const { email, password } = credentials;
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
 
-            try {
-                await connectMongoDB();
-                const user = await User.findOne({ email });
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
 
-                if (!user) {
-                    return null;
-                }
-                
-                const passwordMatch = await bcrypt.compare(password, user.password);
-
-                if (!passwordMatch) {
-                    console.log('Invalid password')
-                    return null;
-                }
-
-                return user;
-
-            } catch (error) {
-                console.log('Error', error)
-                return null;
-            }
-           
+          if (!user) {
+            return null;
           }
-        })
-      ],
-      session: {
-        strategy: 'jwt'
-      },
-    secret: process.env.NEXTAUTH_SECRET,
-    pages: {
-        signIn: '/login',
-    },
-    callbacks: {
-      async jwt({ token, user }) {
-        if (user) {
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (!passwordMatch) {
+            console.log("Invalid password");
+            return null;
+          }
+
           return {
-            ...token,
-            id: user.id,
-            role: user.role
-          }
+            id: user._id.toString(), 
+            name: user.name, 
+            email: user.email,
+          };
+        } catch (error) {
+          console.log("Error", error);
+          return null;
         }
-        return token;
       },
-      async session({ session, token }) {
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
         return {
-          //มันคือการเขียนทับ user อันใหม่เข้าไปใน session.user ของมูลใน client browser
-            ...session,
-            user: {
-              ...session.user,
-              id: token.id,
-              role: token.role
-            }
-        }
+          ...token,
+          id: user.id,
+          role: user.role,
+        };
       }
-    }
-}
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        //มันคือการเขียนทับ user อันใหม่เข้าไปใน session.user ของมูลใน client browser
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          role: token.role,
+        },
+      };
+    },
+  },
+};
 
-const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
